957f9d4869a977ee92baf3367beb501e039bf5c8
pratham-svg <prathampanchariya@gmail.com>
2023-01-10 13:29:08 +0530

    routes and components created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter , Route , Routes , Link} from 'react-router-dom'
+import Home from '../src/Components/Home/Home'
+import About from './Components/About/About';
+import Order from './Components/Order/Order';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Order' element={<Order/>}/>
+        <Route path='/SingUp' element={<SignUp/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
